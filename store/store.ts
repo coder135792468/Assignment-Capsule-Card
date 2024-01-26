23e2b20dd7686cfa5ee7016c22b62635f904fdb9
user45674 <ck56coder@gmail.com>
2024-01-26 13:54:34 +0530

    typescript issue

--- store/store.ts
+++ store/store.ts
@@ -4,7 +4,7 @@ import { capsuleApi } from "./slices/capsuleApiSlice";
 
 export const store = configureStore({
   reducer: {
-    capsule: capsuleSlice.reducer,
+    capsules: capsuleSlice.reducer,
     [capsuleApi.reducerPath]: capsuleApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
